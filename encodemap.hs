21f9336b3f6b75e1c020b484d166e13b8c25bc2d
Bart Massey <bart@cs.pdx.edu>
2010-04-13 19:53:32 -0700

    cleaned up the code some; still is unsat

--- encodemap.hs
+++ encodemap.hs
@@ -1,13 +1,25 @@
 import Data.List
 import Data.Maybe
 
+-- Hardcoded number of map colors
 c = 4
 
--- Given a map with r regions and c colors
--- produce the atom number for region i color j
-region_atom r (i, j) = r * i + j
+-- Given a map with r regions
+-- produce the atom number for "region i is color j"
+region_atom r (i, j) = show $ r * i + j
+--region_atom r (i, j) = "r(" ++ show i ++ "," ++ show j ++ ")"
 
-adjacency_atom r (i, j) = r * c + i * r + j
+-- Given a map with r regions
+-- produce the atom number for "region i is adjacent to region j"
+adjacency_atom r (i, j) = show $ r * c + i * r + j
+--adjacency_atom r (i, j) = "a(" ++ show i ++ "," ++ show j ++ ")"
+
+-- Negate an atom
+neg x = "-" ++ x
+
+-- Fix a clause up for printing
+fix_clause c = c ++ ["0"]
+--fix_clause c = c
 
 -- Produce list of clauses enforcing that region
 -- i has color j drawn from cs if necessary.
@@ -17,7 +29,7 @@ colorize r cs (i, j) =
     where
       c0 = 1 + fromJust (findIndex (== j) cs)
       color_delta d | d == c0 = [region_atom r (i, d)]
-      color_delta d = [-region_atom r (i, d)]
+      color_delta d = [neg (region_atom r (i, d))]
 
 -- Produce list of clauses enforcing that region
 -- i is adjacent to regions in js
@@ -34,7 +46,7 @@ no_two_colors r =
     concatMap ntc' [1..r]
     where
       ntc' i = 
-          concat [[[-region_atom r (i, j), -region_atom r (i, k)]
+          concat [[[neg (region_atom r (i, j)), neg (region_atom r (i, k))]
                         | j <- [1..c], j /= k] | k <- [1..c]]
 
 -- Produce list of clauses enforcing that
@@ -43,8 +55,8 @@ no_adjacencies r =
     concatMap na' [1..c]
     where
       na' k = 
-          concat [[[-adjacency_atom r (i, j),
-                    -region_atom r (i, k), -region_atom r (j, k)]
+          concat [[[neg (adjacency_atom r (i, j)),
+                    neg (region_atom r (i, k)), neg (region_atom r (j, k))]
                        | i <- [1..r], i /= j] | j <- [1..r]]
 
 main = do
@@ -58,9 +70,9 @@ main = do
   let colors = map (\(_ : [c] : _) -> c) map_elems
   let color_names = filter (/= '?') $ nub colors
   let color_list = concatMap (colorize r color_names) $ zip [1..] colors
-  let adjacencies = map (map read . tail . tail) map_elems
+  let adjacencies = map (map read . tail . tail) map_elems :: [[Int]]
   let adjacency_list = concatMap (adjacentize r) $ zip [1..] adjacencies
   let clauses = color_list ++ adjacency_list ++
                 (some_color r) ++ (no_two_colors r) ++ (no_adjacencies r)
   putStrLn $ "p cnf " ++ show (r*c + r*r) ++ " " ++ show (length clauses)
-  putStr $ unlines (map (unwords . map show . (++ [0])) clauses)
+  putStr $ unlines $ map (unwords . fix_clause) clauses
