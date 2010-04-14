d7723856e169c3bd799b38a7d23bd2014727081c
Bart Massey <bart@cs.pdx.edu>
2010-04-13 20:07:56 -0700

    added comments and copyright

--- encodemap.hs
+++ encodemap.hs
@@ -1,3 +1,8 @@
+-- Copyright © 2010 Bart Massey
+
+-- Encode (hand-encoded) sgt-puzzle map problems
+-- as SAT problems in DIMACS format
+
 import Data.List
 import Data.Maybe
 
@@ -60,20 +65,34 @@ no_adjacencies r =
             neg (region_atom r (j, k))]
                | i <- [1..r], j <- [1..r], i /= j]
 
+-- Expects a problem on standard input of the form
+--   region color neighbor neighbor neighbor
+-- where region is a region number counting up from 1
+--       color is a character (up to 4 of them) or ? for no color yet
+--       neighbors are region numbers that are adjacent
+--
+-- Produces a DIMACS SAT encoding of the input problem suitable
+-- for passing to a solver such as MiniSat2
 main = do
+  -- Read the input file
   map_data <- getContents
   let map_elems = map words $ lines $ map_data
+  -- Check the indices
   let indices = map (read . head) map_elems
   if (indices /= [1..length map_elems])
       then error "unsorted map data"
       else return ()
   let r = length indices
+  -- Process the colors
   let colors = map (\(_ : [c] : _) -> c) map_elems
   let color_names = filter (/= '?') $ nub colors
   let color_list = concatMap (colorize r color_names) $ zip [1..] colors
+  -- Process the adjacencies
   let adjacencies = map (map read . tail . tail) map_elems :: [[Int]]
   let adjacency_list = concatMap (adjacentize r) $ zip [1..] adjacencies
+  -- Assemble the list of clauses
   let clauses = color_list ++ adjacency_list ++
                 (some_color r) ++ (no_two_colors r) ++ (no_adjacencies r)
+  -- Print the problem
   putStrLn $ "p cnf " ++ show (r*c + r*r) ++ " " ++ show (length clauses)
   putStr $ unlines $ map (unwords . fix_clause) clauses
